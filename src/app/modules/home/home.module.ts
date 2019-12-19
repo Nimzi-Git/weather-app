eeb72efde25cc723152d5f020e7654a492471da0
Nirman Mandal <itz.nimz@gmail.com>
2019-12-19 14:00:37 +0000

    resolved vulnerabilities related to serialize-javascript lib

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeLandingComponent } from './home-landing/home-landing.component';
-import { HomeRoutingModule } from './home-routing.module';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
